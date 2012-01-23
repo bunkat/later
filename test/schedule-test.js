48d06c137ac0361dced5fac952bcf597f22f0e09
bill <bill@bunkat.com>
2012-01-23 13:19:03 -0800

    refactored Schedule

--- test/schedule-test.js
+++ test/schedule-test.js
@@ -1,36 +1,17 @@
-var schedule = require('../lib/schedule'); //.schedule;
+var schedule = require('../lib/schedule');
 var should = require('should');
 
 describe('Schedule', function() {
 	
-	describe('on', function() {
-		
-		it('should report error on non number input', function() {
-			var s = schedule().onMins('foo');
-			s.errors.should.have.length(1);
-		});
-
-		it('should report error on input too small', function() {
-			var s = schedule().onMins(-1);
-			s.errors.should.have.length(1);
-		});
-
-		it('should report error on input too large', function() {
-			var s = schedule().onMins(61);
-			s.errors.should.have.length(1);
-		});
-
-	});
-
-	describe('atTime', function() {
+	describe('onTime', function() {
 		
 		it('should create a single time restriction with valid input', function() {
-			var s = schedule().atTimes('8:15 am GMT-0800');
+			var s = schedule().onTimes('8:15 am GMT-0800');
 			s.time.should.eql([['08:15:00','08:15:00']]);
 		});
 
 		it('should create a time range restriction with valid input', function() {
-			var s = schedule().atTimes('8:15 pm GMT-0800', '6:15 am GMT-0800');
+			var s = schedule().onTimes('8:15 pm GMT-0800', '6:15 am GMT-0800');
 			s.time.should.eql([['06:15:00','20:15:00']]);
 		});
 
@@ -52,6 +33,26 @@ describe('Schedule', function() {
 		it('should allow multiple restrictions to be chained together', function() {
 			var s = schedule().onMins(5,10).onMins(15,10);
 			s.min.should.eql([[5,10],[10,15]]);
+		});
+
+		it('should return min on non number input', function() {
+			var s = schedule().onMins('foo');
+			s.min.should.eql([[0,0]]);
+		});
+
+		it('should return min on input too small', function() {
+			var s = schedule().onMins(-1);
+			s.min.should.eql([[0,0]]);
+		});
+
+		it('should return max on input too large', function() {
+			var s = schedule().onMins(61);
+			s.min.should.eql([[59,59]]);
+		});
+
+		it('should return max on input too large', function() {
+			var s = schedule().onMins(61, 5);
+			s.min.should.eql([[5,59]]);
 		});
 
 	});
