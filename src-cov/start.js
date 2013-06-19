e41e70d578adccd415e75de1a42b59a161b64b49
Bill Scheidel <bill@bunkat.com>
2013-06-19 15:51:20 -0700

    added src-cov to .gitignore

--- src-cov/start.js
+++
@@ -1,9 +0,0 @@
-/**
-* Later.js 0.1.0
-* (c) 2013 Bill, BunKat LLC.
-* Later is freely distributable under the MIT license.
-* For all details and documentation:
-*     http://bunkat.github.com/later
-*/
-later = (function() {
-  var later = {version: "0.1.0"}; // semver
